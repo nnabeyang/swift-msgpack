cedb8667acbaa69421ee366af7ea382d798a3789
nnabeyang <nabeyang@gmail.com>
2022-08-14 20:48:48 +0900

    Remove unnecessary methods

--- Sources/SwiftMsgpack/MsgPackDecoder.swift
+++ Sources/SwiftMsgpack/MsgPackDecoder.swift
@@ -103,54 +103,6 @@ private extension _MsgPackDecoder {
         ))
     }
 
-    func unbox(_ value: MsgPackValue, as type: Int.Type) throws -> Int? {
-        try unboxInt(value, as: type)
-    }
-
-    func unbox(_ value: MsgPackValue, as type: Int8.Type) throws -> Int8? {
-        try unboxInt(value, as: type)
-    }
-
-    func unbox(_ value: MsgPackValue, as type: Int16.Type) throws -> Int16? {
-        try unboxInt(value, as: type)
-    }
-
-    func unbox(_ value: MsgPackValue, as type: Int32.Type) throws -> Int32? {
-        try unboxInt(value, as: type)
-    }
-
-    func unbox(_ value: MsgPackValue, as type: Int64.Type) throws -> Int64? {
-        try unboxInt(value, as: type)
-    }
-
-    func unbox(_ value: MsgPackValue, as type: UInt.Type) throws -> UInt? {
-        try unboxUInt(value, as: type)
-    }
-
-    func unbox(_ value: MsgPackValue, as type: UInt8.Type) throws -> UInt8? {
-        try unboxUInt(value, as: type)
-    }
-
-    func unbox(_ value: MsgPackValue, as type: UInt16.Type) throws -> UInt16? {
-        try unboxUInt(value, as: type)
-    }
-
-    func unbox(_ value: MsgPackValue, as type: UInt32.Type) throws -> UInt32? {
-        try unboxUInt(value, as: type)
-    }
-
-    func unbox(_ value: MsgPackValue, as type: UInt64.Type) throws -> UInt64? {
-        try unboxUInt(value, as: type)
-    }
-
-    func unbox(_ value: MsgPackValue, as type: Float.Type) throws -> Float? {
-        try unboxFloat(value, as: type)
-    }
-
-    func unbox(_ value: MsgPackValue, as type: Double.Type) throws -> Double? {
-        try unboxFloat(value, as: type)
-    }
-
     func unboxFloat<T: BinaryFloatingPoint & DataNumber>(_ value: MsgPackValue, as type: T.Type) throws -> T? {
         if value == .Nil {
             return nil
@@ -284,51 +236,51 @@ private struct _MsgPackSingleValueDecodingContainer: SingleValueDecodingContaine
     }
 
     func decode(_ type: Double.Type) throws -> Double {
-        try decoder.unbox(value, as: type)!
+        try decoder.unboxFloat(value, as: type)!
     }
 
     func decode(_ type: Float.Type) throws -> Float {
-        try decoder.unbox(value, as: type)!
+        try decoder.unboxFloat(value, as: type)!
     }
 
     func decode(_ type: Int.Type) throws -> Int {
-        try decoder.unbox(value, as: type)!
+        try decoder.unboxInt(value, as: type)!
     }
 
     func decode(_ type: Int8.Type) throws -> Int8 {
-        try decoder.unbox(value, as: type)!
+        try decoder.unboxInt(value, as: type)!
     }
 
     func decode(_ type: Int16.Type) throws -> Int16 {
-        try decoder.unbox(value, as: type)!
+        try decoder.unboxInt(value, as: type)!
     }
 
     func decode(_ type: Int32.Type) throws -> Int32 {
-        try decoder.unbox(value, as: type)!
+        try decoder.unboxInt(value, as: type)!
     }
 
     func decode(_ type: Int64.Type) throws -> Int64 {
-        try decoder.unbox(value, as: type)!
+        try decoder.unboxInt(value, as: type)!
     }
 
     func decode(_ type: UInt.Type) throws -> UInt {
-        try decoder.unbox(value, as: type)!
+        try decoder.unboxUInt(value, as: type)!
     }
 
     func decode(_ type: UInt8.Type) throws -> UInt8 {
-        try decoder.unbox(value, as: type)!
+        try decoder.unboxUInt(value, as: type)!
     }
 
     func decode(_ type: UInt16.Type) throws -> UInt16 {
-        try decoder.unbox(value, as: type)!
+        try decoder.unboxUInt(value, as: type)!
     }
 
     func decode(_ type: UInt32.Type) throws -> UInt32 {
-        try decoder.unbox(value, as: type)!
+        try decoder.unboxUInt(value, as: type)!
     }
 
     func decode(_ type: UInt64.Type) throws -> UInt64 {
-        try decoder.unbox(value, as: type)!
+        try decoder.unboxUInt(value, as: type)!
     }
 
     func decode<T>(_ type: T.Type) throws -> T where T: Decodable {
