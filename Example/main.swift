fc3afc074b6acb45ead63b55da2d0ec1a8f1b846
nnabeyang <nabeyang@gmail.com>
2022-08-14 21:29:14 +0900

    Update comments in example

--- Example/main.swift
+++ Example/main.swift
@@ -26,7 +26,15 @@ let out = try! decoder.decode(Landmark.self, from: data)
 let any = try! decoder.decode(AnyCodable.self, from: data)
 
 print([UInt8](data))
-// [131, 164, 110, 97, 109, 101, 173, 77, 111, 106, 97, 118, 101, 32, 68, 101, 115, 101, 114, 116, 172, 102, 111, 117, 110, 100, 105, 110, 103, 89, 101, 97, 114, 0, 168, 108, 111, 99, 97, 116, 105, 111, 110, 130, 168, 108, 97, 116, 105, 116, 117, 100, 101, 203, 64, 65, 129, 104, 180, 245, 63, 179, 169, 108, 111, 110, 103, 105, 116, 117, 100, 101, 203, 192, 92, 222, 219, 61, 61, 120, 49]
+// [131, 164, 110, 97, 109, 101, 173, 77, 111, 106,
+//  97, 118, 101, 32, 68, 101, 115, 101, 114, 116,
+//  172, 102, 111, 117, 110, 100, 105, 110, 103, 89,
+//  101, 97, 114, 0, 168, 108, 111, 99, 97, 116,
+//  105, 111, 110, 130, 168, 108, 97, 116, 105, 116,
+//  117, 100, 101, 203, 64, 65, 129, 104, 180, 245,
+//  63, 179, 169, 108, 111, 110, 103, 105, 116, 117,
+//  100, 101, 203, 192, 92, 222, 219, 61, 61, 120,
+//  49]
 
 print(out)
 // Landmark(
@@ -40,14 +48,14 @@ print(out)
 
 print(any)
 // AnyCodable(
-//   [
-//     AnyCodable(location): AnyCodable(
-//       [
-//         AnyCodable(longitude): AnyCodable(-115.4821313),
-//         AnyCodable(latitude): AnyCodable(35.0110079)
-//       ]
-//     ),
-//     AnyCodable(foundingYear): AnyCodable(0),
-//     AnyCodable(name): AnyCodable(Mojave Desert)
-//   ]
+//     [
+//         AnyCodable("foundingYear"): AnyCodable(0),
+//         AnyCodable("name"): AnyCodable("Mojave Desert"),
+//         AnyCodable("location"): AnyCodable(
+//             [
+//                 AnyCodable("longitude"): AnyCodable(-115.4821313),
+//                 AnyCodable("latitude"): AnyCodable(35.0110079),
+//             ]
+//         ),
+//     ]
 // )
